7be8ff277b55c16934b20a1b70ec783b504bff02
Cristian Pardo <cristianpardo044@gmail.com>
2023-12-07 01:32:22 -0500

    modal mas info

--- src/components/List/items.list.jsx
+++ src/components/List/items.list.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Modal,
+} from "react-native";
 import { Card, Text, Button } from "react-native-paper";
 import { Ionicons } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
 import { URL_APLIED_PROVIDER } from "../../data/CONSTANT_DATA";
 import { ApiPut } from "../../hooks/Api.hook";
 export function ItemsList(props) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const containerStyle = {
+    backgroundColor: "black",
+    padding: 20,
+    height: "50%",
+    width: "80%",
+    borderRadius: 10,
+  };
+
   console.log("Props");
   console.log(props);
   const user = useSelector((state) => state.user);
@@ -39,12 +54,53 @@ export function ItemsList(props) {
             </Text>
           </ScrollView>
         </View>
-        <TouchableOpacity onPress={() => console.log("On")}>
+        <TouchableOpacity onPress={() => setIsModalVisible(true)}>
           <Text style={{ fontSize: 12, fontWeight: "bold" }}>Mas Info</Text>
         </TouchableOpacity>
         <Card.Actions style={{ padding: 0 }}>
           <Button onPress={() => Aplicar(props._id)}>Aplicar</Button>
         </Card.Actions>
+
+        <Modal
+          visible={isModalVisible}
+          transparent={true}
+          style={styles.modal}
+          animationType="slide"
+        >
+          <View style={styles.modalContent}>
+            <Text
+              variant="titleMedium"
+              style={{ fontSize: "20px", padding: "6px" }}
+            >
+              Información de la oferta
+            </Text>
+            <Text style={styles.modalText}>
+              {" "}
+              Dirección: {props.id_property.direccion}
+            </Text>
+            <Text style={styles.modalText}>
+              {" "}
+              Tipo de propiedad: {props.id_property.tipoPropiedad}
+            </Text>
+            <Text style={styles.modalText}>
+              {" "}
+              Servicio: {props.id_Category_service.Nombre_Categoria}
+            </Text>
+            <Text style={styles.modalText}>
+              {" "}
+              Descripción: {props.description}
+            </Text>
+            <Text style={styles.modalText}> Estado: {props.state}</Text>
+            <Button
+              style={styles.modalButton}
+              title="Close"
+              color="midnightblue"
+              onPress={() => setIsModalVisible(false)}
+            >
+              Cerrar
+            </Button>
+          </View>
+        </Modal>
       </Card.Content>
     </Card>
   );
@@ -100,5 +156,28 @@ const styles = StyleSheet.create({
     paddingRight: 5,
     marginTop: 20,
     position: "relative",
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+  },
+  modalContent: {
+    backgroundColor: "#e2e2e2",
+    padding: 40,
+    width: "80%",
+    borderRadius: 10,
+    elevation: 5,
+    alignItems: "center",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: "-50%" }, { translateY: "-50%" }],
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "black",
+  },
+  modalButton: {
+    marginTop: 10,
   },
 });
